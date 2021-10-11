4a9df709fe46c332b32120f35b19d117c66bf756
Viktor-Ryabov <84191251+Viktor-Ryabov@users.noreply.github.com>
2021-10-11 20:09:47 +0300

    added mobile styles for 1st block

--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,48 @@ const mainMenuBlock = document.querySelector(".header__list");
 const logo = document.querySelector(".header__logoButton");
 const colseIconHeader = document.querySelector("#colseIconHeader");
 
-function addClassList(element, newClassList){
-    element.classList.add(newClassList);
+const subscribeInput = document.querySelector("#singUpInput");
+const singUpButton = document.querySelector("#singUpButton");
+
+function addClassList(element, newClassList) {
+  element.classList.add(newClassList);
 }
-function removeClassList(element, removableClassList){
-    element.classList.remove(removableClassList);
+function removeClassList(element, removableClassList) {
+  element.classList.remove(removableClassList);
 }
 
 function openHeaderMenu(button) {
-    button.addEventListener("click", function(){
-        addClassList(logo, "effects_notVisible");
-        addClassList(burgerMenuButton, "effects_notVisible");
-        addClassList(mainMenuBlock, "effects_visible");
-        addClassList(colseIconHeader, "effects_visible");
-        console.log("re-re");
-    });
+  button.addEventListener("click", function () {
+    addClassList(logo, "effects_notVisible");
+    addClassList(burgerMenuButton, "effects_notVisible");
+    addClassList(mainMenuBlock, "effects_visible");
+    addClassList(colseIconHeader, "effects_visible");
+    console.log("re-re");
+  });
 }
 openHeaderMenu(burgerMenuButton);
 
 function colseHeaderMenu(button) {
-    button.addEventListener("click", function(){
-        removeClassList(logo, "effects_notVisible");
-        removeClassList(burgerMenuButton, "effects_notVisible");
-        removeClassList(mainMenuBlock, "effects_visible");
-        removeClassList(colseIconHeader, "effects_visible");
-        console.log("re-re");
-    });
+  button.addEventListener("click", function () {
+    removeClassList(logo, "effects_notVisible");
+    removeClassList(burgerMenuButton, "effects_notVisible");
+    removeClassList(mainMenuBlock, "effects_visible");
+    removeClassList(colseIconHeader, "effects_visible");
+    console.log("re-re");
+  });
 }
 colseHeaderMenu(colseIconHeader);
+
+function addInputListener(input) {
+  input.addEventListener("click", () => (input.value = ""));
+}
+addInputListener(subscribeInput);
+
+function sendEmailForSubscribe(button, input) {
+  button.addEventListener("click", function () {
+    button.textContent = "Спасибо!";
+    input.value = "";
+  });
+}
+
+sendEmailForSubscribe(singUpButton, subscribeInput);
