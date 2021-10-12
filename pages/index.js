3fb4c12acdeccd11b15aeda1a159ac50fdd73fe5
Viktor-Ryabov <iprva@ya.ru>
2021-10-12 20:19:43 +0300

    finished

--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,59 @@ const mainMenuBlock = document.querySelector(".header__list");
 const logo = document.querySelector(".header__logoButton");
 const colseIconHeader = document.querySelector("#colseIconHeader");
 
-function addClassList(element, newClassList){
-    element.classList.add(newClassList);
+const subscribeInput = document.querySelector("#singUpInput");
+const singUpButton = document.querySelector("#singUpButton");
+const singUpButton1 = document.querySelector(".studyComposition__button");
+const singUpButton2 = document.querySelector(".searchingOrders__button");
+const singUpBlock = document.querySelectorAll(".singUp");
+
+function addClassList(element, newClassList) {
+  element.classList.add(newClassList);
 }
-function removeClassList(element, removableClassList){
-    element.classList.remove(removableClassList);
+function removeClassList(element, removableClassList) {
+  element.classList.remove(removableClassList);
 }
 
 function openHeaderMenu(button) {
-    button.addEventListener("click", function(){
-        addClassList(logo, "effects_notVisible");
-        addClassList(burgerMenuButton, "effects_notVisible");
-        addClassList(mainMenuBlock, "effects_visible");
-        addClassList(colseIconHeader, "effects_visible");
-        console.log("re-re");
-    });
+  button.addEventListener("click", function () {
+    addClassList(logo, "effects_notVisible");
+    addClassList(burgerMenuButton, "effects_notVisible");
+    addClassList(mainMenuBlock, "effects_visible");
+    addClassList(colseIconHeader, "effects_visible");
+    console.log("re-re");
+  });
 }
 openHeaderMenu(burgerMenuButton);
 
 function colseHeaderMenu(button) {
-    button.addEventListener("click", function(){
-        removeClassList(logo, "effects_notVisible");
-        removeClassList(burgerMenuButton, "effects_notVisible");
-        removeClassList(mainMenuBlock, "effects_visible");
-        removeClassList(colseIconHeader, "effects_visible");
-        console.log("re-re");
-    });
+  button.addEventListener("click", function () {
+    removeClassList(logo, "effects_notVisible");
+    removeClassList(burgerMenuButton, "effects_notVisible");
+    removeClassList(mainMenuBlock, "effects_visible");
+    removeClassList(colseIconHeader, "effects_visible");
+    console.log("re-re");
+  });
 }
 colseHeaderMenu(colseIconHeader);
+
+function addInputListener(input) {
+  input.addEventListener("click", () => (input.value = ""));
+}
+addInputListener(subscribeInput);
+
+function sendEmailForSubscribe(button, input) {
+  button.addEventListener("click", function () {
+    button.textContent = "Спасибо!";
+    input.value = "";
+  });
+}
+
+sendEmailForSubscribe(singUpButton, subscribeInput);
+
+function goToSingUpBlock(button){
+  button.addEventListener("click", function(){
+    window.scrollTo( 0, 9991000 );
+  });
+}
+goToSingUpBlock(singUpButton1);
+goToSingUpBlock(singUpButton2);
